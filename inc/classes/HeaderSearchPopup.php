bdf6b9bb1babb4141485589c92ff0888b057e743
robmccormack89 <robertmccormack147@gmail.com>
2021-06-22 22:26:49 +0100

    remove plugin_url

--- inc/classes/HeaderSearchPopup.php
+++ inc/classes/HeaderSearchPopup.php
@@ -15,18 +15,8 @@ class HeaderSearchPopup extends Timber {
     // timber stuff. the usual stuff
     add_filter('timber/twig', array($this, 'add_to_twig'));
     add_filter('timber/context', array($this, 'add_to_context'));
-    
-    // enqueue plugin assets
-    add_action('wp_enqueue_scripts', array($this, 'header_search_assets'));
 
     add_action('rmcc_after_header', 'header_search_popup', 20);
-  }
-  
-  public function header_search_assets() {
-    // wp_enqueue_style(
-    //   header-search',
-    //   HEADER_SEARCH_URL . 'public/css/header-search.css'
-    // );
   }
   
   public function add_to_twig($twig) { 
@@ -35,7 +25,6 @@ class HeaderSearchPopup extends Timber {
   }
 
   public function add_to_context($context) {
-    $context['HEADER_SEARCH_URL'] = MINICART_POPUP_URL;
     return $context;    
   }
 
