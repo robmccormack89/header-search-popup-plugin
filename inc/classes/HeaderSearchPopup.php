24b4872d898e31c98e38091d232994cfc13de349
robmccormack89 <robertmccormack147@gmail.com>
2021-06-24 00:44:04 +0100

    Twig_Extension_StringLoader fix

--- inc/classes/HeaderSearchPopup.php
+++ inc/classes/HeaderSearchPopup.php
@@ -20,7 +20,9 @@ class HeaderSearchPopup extends Timber {
   }
   
   public function add_to_twig($twig) { 
-    $twig->addExtension(new \Twig_Extension_StringLoader());
+    if(!class_exists('Twig_Extension_StringLoader')){
+      $twig->addExtension(new Twig_Extension_StringLoader());
+    }
     return $twig;
   }
 
